cb659bc8ea2a2dbaae68ae35de7ee9b0a94e7d14
MrKireko <mrkireko@gmail.com>
2014-12-11 21:25:05 +0100

    Just basic stuff

---
+++ index.js
@@ -0,0 +1,15 @@
+var num = Math.floor((Math.random() * 6) + 1);
+
+if(num == 1){
+	document.getElementById("pic1").src = "pic1.jpg";
+}else if(num == 2){
+	document.getElementById("pic1").src = "ice1.jpg";
+}else if(num == 3){
+	document.getElementById("pic1").src = "ice2.jpg";
+}else if(num == 4){
+	document.getElementById("pic1").src = "ice3.png";
+}else if(num == 5){
+	document.getElementById("pic1").src = "ice4.png";
+}else if(num == 6){
+	document.getElementById("pic1").src = "ice5.png";
+}
