ab89c3fe2b73cb40aa65aabcf109e7afed19ef4d
MrKireko <mrkireko@gmail.com>
2014-12-11 21:31:20 +0100

    improved image loading

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ var num = Math.floor((Math.random() * 6) + 1);
 
 if(num == 1){
 	document.getElementById("pic1").src = "pic1.jpg";
+	document.getElementById("pic1").style.display = "inline";
 }else if(num == 2){
 	document.getElementById("pic1").src = "ice1.jpg";
+	document.getElementById("pic1").style.display = "inline";
 }else if(num == 3){
 	document.getElementById("pic1").src = "ice2.jpg";
+	document.getElementById("pic1").style.display = "inline";
 }else if(num == 4){
 	document.getElementById("pic1").src = "ice3.png";
+	document.getElementById("pic1").style.display = "inline";
 }else if(num == 5){
 	document.getElementById("pic1").src = "ice4.png";
+	document.getElementById("pic1").style.display = "inline";
 }else if(num == 6){
 	document.getElementById("pic1").src = "ice5.png";
+	document.getElementById("pic1").style.display = "inline";
 }
