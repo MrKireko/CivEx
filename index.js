f94ee3a22bc579298c633680c10a7b6f561785d7
MrKireko <mrkireko@gmail.com>
2014-12-13 01:29:39 +0100

    1.3

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 setInterval($('#slide').cycle(), 100);
+
+$("#mapnav").click(function(){
+	if($("#mapnav").hasClass("nabla")){
+		$("#mapnav").html("&nbsp; MAP &Delta; &nbsp;");
+		$("#mapnav").removeClass("nabla");
+		$("#mapmenu").css("display", "block");
+		
+	}else{
+		$("#mapnav").html("&nbsp; MAP &nabla; &nbsp;");
+		$("#mapnav").addClass("nabla");
+		$("#mapmenu").css("display", "none");
+	}
+});
